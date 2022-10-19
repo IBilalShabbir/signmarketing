35de535fb36e86fbc35f415262dcd47fb2c1cf4c
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-10-19 18:49:33 +0500

    project section done

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,4 @@
 export { default as react } from "./react.svg";
 export { default as aboutbg } from "./aboutbg.png";
+export { default as swiperimg1 } from "./swiperimg1.png";
+export { default as swiperimg2 } from "./swiperimg2.png";
