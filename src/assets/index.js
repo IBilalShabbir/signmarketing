bdea1b6a48ec09302e4c19ea408718c2325b70ae
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-10-22 13:17:06 +0500

    your choice section half done

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,3 +2,6 @@ export { default as react } from "./react.svg";
 export { default as aboutbg } from "./aboutbg.png";
 export { default as swiperimg1 } from "./swiperimg1.png";
 export { default as swiperimg2 } from "./swiperimg2.png";
+export { default as swiper123 } from "./swiper123.png";
+export { default as yourchoicebg } from "./yourchoicebg.png";
+export { default as yourchoicewrapper } from "./yourchoicewrapper.png";
